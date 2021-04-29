6fa77c393b6e75055e381556f021905574955e4b
edougher <ejdougher@gmail.com>
2021-04-29 18:56:25 -0400

    CatList is displaying images

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,27 @@
 // write your CatList component here
+import  React, { Component } from 'react';
+
+class CatList extends Component{
+
+    displayCats = (cats) => {
+        console.log(cats);
+        return cats.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
+    }
+
+    componentDidMount(){
+        console.log(this.props.catPics);
+    }
+
+    render(){
+        console.log(this.props.catPics);
+        return(
+            <div>
+                {this.displayCats(this.props.catPics)}
+            </div>
+        )
+
+        
+    }
+}
+
+export default CatList
